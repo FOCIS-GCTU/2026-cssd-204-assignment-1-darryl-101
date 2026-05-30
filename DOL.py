94c4670b9fbc7f269af0d6c1d02fd2cf3270e5d0
darryl-101 <2425402784@live.gctu.edu.gh>
2026-05-30 19:47:03 +0000

    Add files via upload

---
+++ DOL.py
@@ -0,0 +1,38 @@
+# File: DOL.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Darryl Otchere Larbi
+# STUDENT ID: 2425402784
+#GROUP :A
+# Email: 2425402784@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor,Darryl Otchere Larbi, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Prints my initials DOL in large stylized letters.
+
+    print()
+
+    print("...DOL")
+
+    print()
+
+    print("...DDDDDDDD..............OOOOOOOO..........LLL..............")
+    print("...DDDDDDDDDD...........OOOOOOOOOO.........LLL..............")
+    print("...DD.......DDD........OO........OO........LLL..............")
+    print("...DD........DD........OO........OO........LLL..............")
+    print("...DD........DD........OO........OO........LLL..............")
+    print("...DD........DD........OO........OO........LLL..............")
+    print("...DD........DD........OO........OO........LLL..............")
+    print("...DD.......DDD........OO........OO........LLL..............")
+    print("...DDDDDDDDDD.....**....OOOOOOOOOO....**...LLLLLLLLLLLL...**")
+    print("...DDDDDDDD.......**.....0000000O.....**...LLLLLLLLLLLL...**")
+
+    print()
+
+
+main()
